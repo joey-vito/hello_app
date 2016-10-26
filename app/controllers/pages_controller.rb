a7d74dece585bb4cab6246394d890d5e0030cb40
Joey Johnson <joey.johnson7@icloud.com>
2016-10-26 15:58:29 -0400

    Adding pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  def about_us
+
+  end
+  def hello
+    render html: "hello, world!"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  # get '/patients/:id', to: 'patients#show', as: 'patient'
+  get '/about_us', to: 'pages#about_us', as: 'about_us'
+  root 'pages#index'
 end
